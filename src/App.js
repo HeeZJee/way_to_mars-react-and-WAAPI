801dea9cba96ebc8329ff8b145d23a85f532c3cb
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-07-28 21:39:00 +0500

    basic structure created

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello, World!
+      <img id="earth" src="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/earth.svg" srcSet="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/earth.svg 2x" alt="Earth" title="Planet Earth" />
+
+      <img id="mars" src="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/mars.svg" srcSet="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/mars.svg 2x" alt="Mars" title="Planet Mars" />
+
+
+      <div class='rocket'>
+
+        <img id="flame" src="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/rocket-flame.svg" srcSet="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/rocket-flame.svg 2x" alt="" title="Starship" />
+        <img id="rocket-body" src="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/rocket.svg" srcSet="https://black-holes-interactives.onlea.org/escapeVelocity/assets/images/rocket.svg 2x" alt="" title="Starship" />
+      </div>
+
+
     </div>
   );
 }
 
-export default App;
+export default App; 
